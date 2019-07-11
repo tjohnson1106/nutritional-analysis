3bb9a40d7c0ed23f54666b4b34fa2875e8263fe6
Thomas Johnson <tjohnson1106@gmail.com>
2019-07-11 14:35:40 -0400

    develop

---
+++ src/Main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Main: React.FC = () => {
+  const APP_ID = "f5466f40";
+  const APP_KEY = "585b1769b46056c2888b329f37bc0453";
+
+  return <div>Main</div>;
+};
