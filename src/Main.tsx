4071fe3d1224c7f62cbee083205fd160fa57d240
Thomas Johnson <tjohnson1106@gmail.com>
2019-07-11 15:11:09 -0400

    created set recipes use effect

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Main: React.FC = () => {
   const APP_ID = "f5466f40";
   const APP_KEY = "585b1769b46056c2888b329f37bc0453";
 
-  return <div>Main</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
